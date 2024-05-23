8bf2a8e6c9f0ca7fc7586b3a037882b2aa9460b8
marciehenderson <marciehenderson.personal@gmail.com>
2024-05-23 16:24:59 -0300

    Improved overall look and feel of web pages.

--- src/frontend/app.ts
+++ src/frontend/app.ts
@@ -1,41 +1,53 @@
+// Matieral Web Components
+import '@material/web/button/elevated-button';
+import '@material/web/list/list';
+import '@material/web/list/list-item';
+import '@material/web/tabs/tabs';
+import '@material/web/tabs/primary-tab';
+import '@material/web/tabs/secondary-tab';
+import '@material/web/textfield/outlined-text-field';
+// Compile with: npx tsc src/frontend/app.ts && npx rollup -p @rollup/plugin-node-resolve ./dist/app.js -o ./dist/bundle.js
 // View Functions
 const accountView = (): void => {
     const account = document.createElement('account');
     account.innerHTML = `
-        <h2>Login</h2>
-        <div>
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username">
+        <div class="view-top-padding"></div>
+        <div class="view-input-container">
+            <md-outlined-text-field label="Username" type="text">
+            </md-outlined-text-field>
+            <md-outlined-text-field label="Password" type="password">
+            </md-outlined-text-field>
         </div>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password">
-        </div>
-        <button type="submit">Login</button>
+        <md-elevated-button type="submit">Login</md-elevated-button>
     `;
+    account.classList.add('view');
     document.getElementById('app')!.appendChild(account);
 };
 const actionsView = (): void => {
     const actions = document.createElement('div');
     actions.innerHTML = `
-        <h2>Actions</h2>
-        <button id="create">Create Record</button>
-        <button id="modify">Modify Record</button>
-        <button id="view">View Record</button>
+        <md-tabs>
+            <md-secondary-tab role="button" id="create">Create Record</md-secondary-tab>
+            <md-secondary-tab role="button" id="modify">Modify Record</md-secondary-tab>
+            <md-secondary-tab role="button" id="view">View Record</md-secondary-tab>
+        </md-tabs>
+        <div class="view-top-padding"></div>
     `;
+    actions.classList.add('view');
     document.getElementById('app')!.appendChild(actions);
 };
 const supportView = (): void => {
     const support = document.createElement('div');
     support.innerHTML = `
-        <h2>Support</h2>
-        <ul>
-            <li>Phone: 555-555-5555</li>
-            <li>Email: example@example.com</li>
-            <li>Address: 1234 Example St.</li>
-            <li>Hours: 9am-5pm</li>
-        </ul>
+        <div class="view-top-padding"></div>
+        <md-list>
+            <md-list-item>Phone: 555-555-5555</md-list-item>
+            <md-list-item>Email: example@example.com</md-list-item>
+            <md-list-item>Address: 1234 Example St.</md-list-item>
+            <md-list-item>Hours: 9am-5pm</md-list-item>
+        </md-list>
     `;
+    support.classList.add('view');
     document.getElementById('app')!.appendChild(support);
 };
 
