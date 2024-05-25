7689b023529fc6b6c7e945115415519de93008cb
marciehenderson <marciehenderson.personal@gmail.com>
2024-05-25 16:14:19 -0300

    Created basic record creation form ui.

--- src/frontend/app.ts
+++ src/frontend/app.ts
@@ -1,4 +1,5 @@
 // Matieral Web Components
+import '@material/web/icon/icon';
 import '@material/web/button/elevated-button';
 import '@material/web/list/list';
 import '@material/web/list/list-item';
@@ -6,8 +7,12 @@ import '@material/web/tabs/tabs';
 import '@material/web/tabs/primary-tab';
 import '@material/web/tabs/secondary-tab';
 import '@material/web/textfield/outlined-text-field';
+import '@material/web/textfield/filled-text-field';
+import '@material/web/select/outlined-select';
+import '@material/web/select/select-option';
 // Compile with: npx tsc src/frontend/app.ts && npx rollup -p @rollup/plugin-node-resolve ./dist/app.js -o ./dist/bundle.js
 // View Functions
+// Account View - User Login Page
 const accountView = (): void => {
     const account = document.createElement('account');
     account.innerHTML = `
@@ -23,19 +28,86 @@ const accountView = (): void => {
     account.classList.add('view');
     document.getElementById('app')!.appendChild(account);
 };
-const actionsView = (): void => {
+// Actions View - Create, Modify, View Records
+const actionsView = (subhash: string): void => {
     const actions = document.createElement('div');
     actions.innerHTML = `
         <md-tabs>
-            <md-secondary-tab role="button" id="create">Create Record</md-secondary-tab>
-            <md-secondary-tab role="button" id="modify">Modify Record</md-secondary-tab>
-            <md-secondary-tab role="button" id="view">View Record</md-secondary-tab>
+            <md-secondary-tab role="button" onclick="window.location.hash='actions+create'" id="create">Create</md-secondary-tab>
+            <md-secondary-tab role="button" onclick="window.location.hash='actions+modify'" id="modify">Modify</md-secondary-tab>
+            <md-secondary-tab role="button" onclick="window.location.hash='actions+view'" id="view">View</md-secondary-tab>
         </md-tabs>
-        <div class="view-top-padding"></div>
     `;
     actions.classList.add('view');
     document.getElementById('app')!.appendChild(actions);
+    const actionsSubView = document.createElement('div');
+    switch (subhash) {
+        default:
+        case 'create':
+            actionsSubView.innerHTML = `
+                <div class="action-indicator-bar">
+                    <div class="active-indicator"></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div class="view-top-padding"></div>
+                <div class="view-input-container">
+                    <md-outlined-text-field label="Patient ID" type="search">
+                        <md-icon slot="trailing-icon">search</md-icon>
+                    </md-outlined-text-field>
+                    <md-outlined-text-field label="Location ID" type="search">
+                        <md-icon slot="trailing-icon">search</md-icon>
+                    </md-outlined-text-field>
+                    <md-outlined-select label="Type">
+                        <md-select-option value="1">
+                            <div slot="headline">Check-Up</div>
+                        </md-select-option>
+                        <md-select-option value="2">
+                            <div slot="headline">Annual</div>
+                        </md-select-option>
+                        <md-select-option value="3">
+                            <div slot="headline">Blood-Work</div>
+                        </md-select-option>
+                        <md-select-option value="4">
+                            <div slot="headline">Vaccination</div>
+                        </md-select-option>
+                        <md-select-option value="5">
+                            <div slot="headline">Emergency</div>
+                        </md-select-option>
+                    </md-outlined-select>
+                    <md-outlined-text-field label="Notes" type="textarea">
+                    </md-outlined-text-field>
+                    <md-elevated-button type="submit">Submit</md-elevated-button>
+                </div>
+            `;
+            break;
+        case 'modify':
+            actionsSubView.innerHTML = `
+                <div class="action-indicator-bar">
+                    <div></div>
+                    <div class="active-indicator"></div>
+                    <div></div>
+                </div>
+                <div class="view-top-padding"></div>
+                <h1>Modify Record</h1>
+            `;
+            break;
+        case 'view':
+            actionsSubView.innerHTML = `
+                <div class="action-indicator-bar">
+                    <div></div>
+                    <div></div>
+                    <div class="active-indicator"></div>
+                </div>
+                <div class="view-top-padding"></div>
+                <h1>View Record</h1>
+            `;
+            break;
+    };
+    actionsSubView.classList.add('subview');
+    document.getElementsByClassName('view')[0]!.appendChild(actionsSubView);
 };
+// Support View - Contact Information and User Support
 const supportView = (): void => {
     const support = document.createElement('div');
     support.innerHTML = `
@@ -55,19 +127,19 @@ const supportView = (): void => {
 const showView = (hash: string): void => {
     // clear parent element
     document.getElementById('app')!.innerHTML = '';
+    // separate hash from sub-hash
+    var subhash = hash.split('+')[1];
+    hash = hash.split('+')[0];
     // choose new view based on hash
     switch (hash) {
         case '#account':
             accountView();
             break;
         case '#actions':
-            actionsView();
+            actionsView(subhash);
             break;
         case '#support':
             supportView();
-            break;
-        case '':
-            document.getElementById('app')!.innerHTML = 'Home Page';
             break;
         default:
             document.getElementById('app')!.innerHTML = '404 Not Found';
--- src/frontend/dist/app.js
+++ src/frontend/dist/app.js
@@ -1,3 +1,4 @@
+import '@material/web/icon/icon';
 import '@material/web/button/elevated-button';
 import '@material/web/list/list';
 import '@material/web/list/list-item';
@@ -5,6 +6,9 @@ import '@material/web/tabs/tabs';
 import '@material/web/tabs/primary-tab';
 import '@material/web/tabs/secondary-tab';
 import '@material/web/textfield/outlined-text-field';
+import '@material/web/textfield/filled-text-field';
+import '@material/web/select/outlined-select';
+import '@material/web/select/select-option';
 const accountView = () => {
     const account = document.createElement('account');
     account.innerHTML = `
@@ -20,18 +24,84 @@ const accountView = () => {
     account.classList.add('view');
     document.getElementById('app').appendChild(account);
 };
-const actionsView = () => {
+const actionsView = (subhash) => {
     const actions = document.createElement('div');
     actions.innerHTML = `
         <md-tabs>
-            <md-secondary-tab role="button" id="create">Create Record</md-secondary-tab>
-            <md-secondary-tab role="button" id="modify">Modify Record</md-secondary-tab>
-            <md-secondary-tab role="button" id="view">View Record</md-secondary-tab>
+            <md-secondary-tab role="button" onclick="window.location.hash='actions+create'" id="create">Create</md-secondary-tab>
+            <md-secondary-tab role="button" onclick="window.location.hash='actions+modify'" id="modify">Modify</md-secondary-tab>
+            <md-secondary-tab role="button" onclick="window.location.hash='actions+view'" id="view">View</md-secondary-tab>
         </md-tabs>
-        <div class="view-top-padding"></div>
     `;
     actions.classList.add('view');
     document.getElementById('app').appendChild(actions);
+    const actionsSubView = document.createElement('div');
+    switch (subhash) {
+        default:
+        case 'create':
+            actionsSubView.innerHTML = `
+                <div class="action-indicator-bar">
+                    <div class="active-indicator"></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div class="view-top-padding"></div>
+                <div class="view-input-container">
+                    <md-outlined-text-field label="Patient ID" type="search">
+                        <md-icon slot="trailing-icon">search</md-icon>
+                    </md-outlined-text-field>
+                    <md-outlined-text-field label="Location ID" type="search">
+                        <md-icon slot="trailing-icon">search</md-icon>
+                    </md-outlined-text-field>
+                    <md-outlined-select label="Type">
+                        <md-select-option value="1">
+                            <div slot="headline">Check-Up</div>
+                        </md-select-option>
+                        <md-select-option value="2">
+                            <div slot="headline">Annual</div>
+                        </md-select-option>
+                        <md-select-option value="3">
+                            <div slot="headline">Blood-Work</div>
+                        </md-select-option>
+                        <md-select-option value="4">
+                            <div slot="headline">Vaccination</div>
+                        </md-select-option>
+                        <md-select-option value="5">
+                            <div slot="headline">Emergency</div>
+                        </md-select-option>
+                    </md-outlined-select>
+                    <md-outlined-text-field label="Notes" type="textarea">
+                    </md-outlined-text-field>
+                    <md-elevated-button type="submit">Submit</md-elevated-button>
+                </div>
+            `;
+            break;
+        case 'modify':
+            actionsSubView.innerHTML = `
+                <div class="action-indicator-bar">
+                    <div></div>
+                    <div class="active-indicator"></div>
+                    <div></div>
+                </div>
+                <div class="view-top-padding"></div>
+                <h1>Modify Record</h1>
+            `;
+            break;
+        case 'view':
+            actionsSubView.innerHTML = `
+                <div class="action-indicator-bar">
+                    <div></div>
+                    <div></div>
+                    <div class="active-indicator"></div>
+                </div>
+                <div class="view-top-padding"></div>
+                <h1>View Record</h1>
+            `;
+            break;
+    }
+    ;
+    actionsSubView.classList.add('subview');
+    document.getElementsByClassName('view')[0].appendChild(actionsSubView);
 };
 const supportView = () => {
     const support = document.createElement('div');
@@ -49,18 +119,17 @@ const supportView = () => {
 };
 const showView = (hash) => {
     document.getElementById('app').innerHTML = '';
+    var subhash = hash.split('+')[1];
+    hash = hash.split('+')[0];
     switch (hash) {
         case '#account':
             accountView();
             break;
         case '#actions':
-            actionsView();
+            actionsView(subhash);
             break;
         case '#support':
             supportView();
-            break;
-        case '':
-            document.getElementById('app').innerHTML = 'Home Page';
             break;
         default:
             document.getElementById('app').innerHTML = '404 Not Found';
