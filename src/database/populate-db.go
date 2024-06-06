341a889b1c3fa94f5c3f0100e359fed63b920317
marciehenderson <marciehenderson.personal@gmail.com>
2024-06-06 16:23:03 -0300

    Improved random word generation algorithm.

--- src/database/populate-db.go
+++ src/database/populate-db.go
@@ -13,6 +13,7 @@ import (
 
 // Declare global variables
 var db *sql.DB
+var defaultSeed = []string{"apple", "bingo", "calendar", "dance", "evening", "flower", "goodbye", "hello", "ice", "jungle", "kite", "laptop", "mango", "notebook", "orange", "pencil", "queen", "rabbit", "sunset", "table", "umbrella", "violet", "water", "xylophone", "yellow", "zebra"}
 
 func main() {
 	// Get and set configuration values
@@ -40,11 +41,16 @@ func main() {
 	fmt.Println("Connected to database")
 
 	// Test name generation
-	for i := 0; i < 10; i++ {
+	f, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	for i := 0; i < 100; i++ {
 		name := randomName(3)
 		fmt.Println(name[0], name[1])
 		// save to log file
-		err := os.WriteFile("log.txt", []byte(fmt.Sprintln(name[0], name[1])), 0644)
+		_, err := f.WriteString(fmt.Sprintf("%s %s\n", name[0], name[1]))
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -54,7 +60,7 @@ func main() {
 	// User Table
 	for i := 0; i < 100; i++ {
 		// Generate a random user for the table
-		_, err := db.Exec("INSERT INTO user (category, password_hash, user_hash, permissions) VALUES (?, ?, ?, ?)", randomCategory(), randomWord(10), randomWord(10), json.Number("0"))
+		_, err := db.Exec("INSERT INTO user (category, password_hash, user_hash, permissions) VALUES (?, ?, ?, ?)", randomCategory(), randomWord(10, defaultSeed), randomWord(10, defaultSeed), json.Number("0"))
 		// Log any errors
 		if err != nil {
 			log.Fatal(err)
@@ -69,7 +75,7 @@ func main() {
 	// Patient Table
 	for i := 0; i < 100; i++ {
 		// Generate a random patient for the table
-		_, err := db.Exec("INSERT INTO patient (first_name, last_name, date_of_birth, street_address, contact_number, email) VALUES (?, ?, ?, ?, ?, ?)", randomWord(1), randomWord(1), randomDate(), randomWord(10), "1234567890", randomWord(5)+"@"+randomWord(5)+".com")
+		_, err := db.Exec("INSERT INTO patient (first_name, last_name, date_of_birth, street_address, contact_number, email) VALUES (?, ?, ?, ?, ?, ?)", randomWord(1, defaultSeed), randomWord(1, defaultSeed), randomDate(), randomWord(10, defaultSeed), "1234567890", randomWord(5, defaultSeed)+"@"+randomWord(5, defaultSeed)+".com")
 		// Log any errors
 		if err != nil {
 			log.Fatal(err)
@@ -78,7 +84,7 @@ func main() {
 	// Practitioner Table
 	for i := 0; i < 100; i++ {
 		// Generate a random practitioner for the table
-		_, err := db.Exec("INSERT INTO practitioner (first_name, last_name, date_of_birth, street_address, contact_number, email) VALUES (?, ?, ?, ?, ?, ?)", randomWord(1), randomWord(1), randomDate(), randomWord(10), "1234567890", randomWord(5)+"@"+randomWord(5)+".com")
+		_, err := db.Exec("INSERT INTO practitioner (first_name, last_name, date_of_birth, street_address, contact_number, email) VALUES (?, ?, ?, ?, ?, ?)", randomWord(1, defaultSeed), randomWord(1, defaultSeed), randomDate(), randomWord(10, defaultSeed), "1234567890", randomWord(5, defaultSeed)+"@"+randomWord(5, defaultSeed)+".com")
 		// Log any errors
 		if err != nil {
 			log.Fatal(err)
@@ -87,7 +93,7 @@ func main() {
 	// Location Table
 	for i := 0; i < 100; i++ {
 		// Generate a random location for the table
-		_, err := db.Exec("INSERT INTO location (location_name, street_address, contact_number, email) VALUES (?, ?, ?, ?)", randomWord(1), randomWord(10), "1234567890", randomWord(5)+"@"+randomWord(5)+".com")
+		_, err := db.Exec("INSERT INTO location (location_name, street_address, contact_number, email) VALUES (?, ?, ?, ?)", randomWord(1, defaultSeed), randomWord(10, defaultSeed), "1234567890", randomWord(5, defaultSeed)+"@"+randomWord(5, defaultSeed)+".com")
 		// Log any errors
 		if err != nil {
 			log.Fatal(err)
@@ -96,7 +102,7 @@ func main() {
 	// Record Table
 	for i := 0; i < 100; i++ {
 		// Generate a random record for the table
-		_, err := db.Exec("INSERT INTO record (patient_id, record_date, practitioner_id, location_id, notes, code_cpt, code_icd) VALUES (?, ?, ?, ?, ?, ?, ?)", rand.Intn(100)+1, randomDate(), rand.Intn(100)+1, rand.Intn(100)+1, randomWord(10), randomWord(5), randomWord(5))
+		_, err := db.Exec("INSERT INTO record (patient_id, record_date, practitioner_id, location_id, notes, code_cpt, code_icd) VALUES (?, ?, ?, ?, ?, ?, ?)", rand.Intn(100)+1, randomDate(), rand.Intn(100)+1, rand.Intn(100)+1, randomWord(10, defaultSeed), randomWord(5, defaultSeed), randomWord(5, defaultSeed))
 		// Log any errors
 		if err != nil {
 			log.Fatal(err)
@@ -113,54 +119,108 @@ func main() {
 	}
 }
 
-func likelihood(dictionary []string, seed []string) []float32 {
+// Generates a weighted dictionary from a seed of strings with a given window size.
+// The strength parameter determines how much the weight of a window is increased,
+// and more specifically, the magnitude of difference between likely and unlikely
+// windows. A strength of 0.0 will result in a uniform distribution of all windows.
+func generateWeightedDictionary(seed []string, size int, strength float32) ([]string, []float32) {
+	var dictionary []string
 	var weight []float32
-	size := len(dictionary[0])
-	for i := 0; i < len(dictionary); i++ {
-		// check if all words in the dictionary are the same size
-		if len(dictionary[i]) != size {
-			return nil
+	for i := 0; i < len(seed); i++ {
+		if len(seed[i]) < size {
+			fmt.Printf("Error: seed string (%s) is too short for size of window (%d)\n", seed[i], size)
+			break
 		}
-		var count int
-		for j := 0; j < len(seed); j++ {
-			for k := 0; k < len(seed[j])-size; k++ {
-				// get a window of the seed from the current position
-				window := seed[j][k : k+size]
-				// check if the window matches the current word in the dictionary
-				if window == dictionary[i] {
-					count++
+		for j := 0; j < len(seed[i])-size; j++ {
+			// get a window of the seed from the current position
+			window := seed[i][j : j+size]
+			// check if the window is already in the dictionary
+			found := false
+			// only check the dictionary if it is not empty
+			if len(dictionary) > 0 {
+				for k := 0; k < len(dictionary); k++ {
+					if window == dictionary[k] {
+						// increase the weight of the window based on the strength
+						weight[k] += strength
+						found = true
+						break
+					}
 				}
 			}
-		}
-		// calculate weight of the word based on its frequency in the seed
-		weight = append(weight, float32(count)/float32(len(seed)))
-	}
-	return weight
-}
-
-// generates a random word with the requested number of syllables
-// notably the word is not likely to be a real word, but should
-// be useful for testing purposes.
-func randomWord(syllables int) string {
-	var word string
-	dictionary := []string{"ba", "be", "bi", "bo", "bu", "da", "de", "di", "do", "du", "fa", "fe", "fi", "fo", "fu", "ga", "ge", "gi", "go", "gu", "ha", "he", "hi", "ho", "hu", "ja", "je", "ji", "jo", "ju", "ka", "ke", "ki", "ko", "ku", "la", "le", "li", "lo", "lu", "ma", "me", "mi", "mo", "mu", "na", "ne", "ni", "no", "nu", "pa", "pe", "pi", "po", "pu", "ra", "re", "ri", "ro", "ru", "sa", "se", "si", "so", "su", "ta", "te", "ti", "to", "tu", "va", "ve", "vi", "vo", "vu", "wa", "we", "wi", "wo", "wu", "ya", "ye", "yi", "yo", "yu", "za", "ze", "zi", "zo", "zu"}
-	seed := []string{"alice", "bob", "charlie", "david", "eve", "frank", "grace", "heidi", "ivan", "judy", "kevin", "linda", "mallory", "nancy", "oscar", "peggy", "romeo", "sybil", "trudy", "victor", "walter", "xavier", "yvonne", "zelda"}
-	weight := likelihood(dictionary, seed)
-	// shuffle the dictionary and weight slices
-	for i := range dictionary {
-		j := rand.Intn(i + 1)
-		dictionary[i], dictionary[j] = dictionary[j], dictionary[i]
-		weight[i], weight[j] = weight[j], weight[i]
-	}
-	// make a random word with the requested number of syllables
-	for i := 0; i < syllables; i++ {
-		for j := 0; j < len(dictionary); j++ {
-			if rand.Float32() < weight[j] {
-				// concatenate a random syllable to the word
-				word += dictionary[j]
-				break
+			// add the window to the dictionary if it is not already there
+			// and initialize its associated weight to 1.0
+			if !found {
+				dictionary = append(dictionary, window)
+				weight = append(weight, 1.0)
 			}
 		}
+	}
+	return dictionary, weight
+}
+
+// Selects a window from a weighted dictionary based on the weight of each window.
+func selectWindow(dictionary []string, weight []float32) string {
+	// calculate the total weight of the dictionary
+	var total float32
+	for i := 0; i < len(weight); i++ {
+		total += weight[i]
+	}
+	// generate a random number between 0 and the total weight
+	rand := rand.Float32() * total
+	// select a window based on its weight
+	for i := 0; i < len(weight); i++ {
+		if rand < weight[i] {
+			return dictionary[i]
+		}
+		rand -= weight[i]
+	}
+	return ""
+}
+
+// Reads a file and returns its contents as a string array
+func seedFromFile(path string) []string {
+	// open the file at the given path
+	f, err := os.Open(path)
+	// log any errors
+	if err != nil {
+		log.Fatal(err)
+	}
+	// read the file into a byte slice
+	data := make([]byte, 100)
+	count, err := f.Read(data)
+	// log any errors
+	if err != nil {
+		log.Fatal(err)
+	}
+	// close the file
+	f.Close()
+	// convert data to []string with EOL as delimiter
+	raw := string(data[:count])
+	var seed []string
+	for i := 0; i < len(raw); i++ {
+		// check for the end of the line
+		if raw[i] == '\n' {
+			// append raw slice to seed
+			seed = append(seed, raw[:i])
+			// remove slice from raw
+			raw = raw[i+1:]
+			// reset iterator
+			i = 0
+		}
+	}
+	// return the byte slice as a string
+	return seed
+}
+
+// Generates a random word with the requested number of syllables.
+// Notably the word is not likely to be a real word, but should
+// be useful for testing purposes.
+func randomWord(syllables int, seed []string) string {
+	var word string
+	dictionary, weight := generateWeightedDictionary(seed, 3, 0.5)
+	// make a random word with the requested number of syllables
+	for i := 0; i < syllables; i++ {
+		word += selectWindow(dictionary, weight)
 	}
 	// print the random word to the console
 	fmt.Println(word)
@@ -168,11 +228,13 @@ func randomWord(syllables int) string {
 }
 
 func randomName(max int) [2]string {
+	//seed := []string{"alice", "bob", "charlie", "david", "eve", "frank", "grace", "heidi", "ivan", "judy", "kevin", "linda", "mallory", "nancy", "oscar", "peggy", "romeo", "sybil", "trudy", "victor", "walter", "xavier", "yvonne", "zelda"}
+	seed := seedFromFile("name-seed.txt")
 	var name [2]string
 	// generate a random first name with a random number of syllables
-	name[0] = randomWord(rand.Intn(max) + 1)
+	name[0] = randomWord(rand.Intn(max)+1, seed)
 	// generate a random last name with a random number of syllables
-	name[1] = randomWord(rand.Intn(max) + 1)
+	name[1] = randomWord(rand.Intn(max)+1, seed)
 	return name
 }
 
